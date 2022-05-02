a94775d2d44a4b9b821d4e799922dddb5ae61bff
Thang PHAM <phamthang37@yahoo.com>
2022-05-02 15:34:05 +0200

    Localisation

--- app/src/main/java/com/example/googlemap/MapsActivity.kt
+++ app/src/main/java/com/example/googlemap/MapsActivity.kt
@@ -2,7 +2,10 @@ package com.example.googlemap
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,6 +13,7 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -18,16 +22,24 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.googlemap.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.Marker
+import com.google.maps.android.clustering.ClusterManager
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, GoogleMap.InfoWindowAdapter  {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private val LOCATION_REQ_CODE = 456
-
+    // On definit un array de station que l’on peut recuperer d’un web service
+    private lateinit var stations: ArrayList<Station>
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.purple_200)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_baseline_directions_bike_24, color)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        stations= arrayListOf()
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -83,12 +95,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
+    @SuppressLint("MissingPermission")
     fun afficher(view: View) {
         val lieu = LatLng(45.45, 4.50)
         mMap.addMarker(MarkerOptions().position(lieu).title("Marker in lyon"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(lieu))
         //Pour obtenir une animation plutot qu’un saut on remplace
         mMap.animateCamera(CameraUpdateFactory.newLatLng(lieu));
+//        addMarkers(mMap)
+        addClusteredMarkers(mMap)
+
+        // get current position
+        if (!getLocation()) return
+        val locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location=locationManager.getLastKnownLocation(locationManager.getBestProvider(Criteria(),true)!!)
+        if (location!=null){
+            val position = LatLng(location.latitude, location.longitude)
+            mMap.addMarker(MarkerOptions().position(position).title("Ma position"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
+            Toast.makeText(this,location.longitude.toString()+" "+location.latitude.toString(),Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onInfoWindowClick(marker: Marker) {
@@ -96,9 +122,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
         Toast.makeText(this, marker.title, Toast.LENGTH_SHORT).show();
     }
 
-    override fun getInfoWindow(p0: Marker): View? {
+    override fun getInfoWindow(marker: Marker): View? {
         // 1. Get tag
-        val station1=Station("Title", LatLng(45.45, 4.50),"Adresse")
+        val station = marker?.tag as? Station ?: return null
 
         // 2. Inflate view and set title, address, and rating
         val view = LayoutInflater.from(applicationContext).inflate(
@@ -106,10 +132,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
         )
         view.findViewById<TextView>(
             R.id.text_view_title
-        ).text = station1.name
+        ).text = station.name
         view.findViewById<TextView>(
             R.id.text_view_address
-        ).text = station1.address
+        ).text = station.address
         return view
     }
 
@@ -139,5 +165,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
             Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
                 ))
+    }
+
+    // on definit une methode qui va ajouter des markers aux points d’interets
+    private fun addMarkers(googleMap: GoogleMap) {
+        addItems()
+        stations.forEach { station ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(station.name)
+                    .position(station.latLng)
+                    .icon(bicycleIcon)
+
+            )
+            //On ajoute la station comme tag au marker pour l’afficher
+            marker?.tag=station
+        }
+        //On definit la position de depart de la camera qui peut etre le point initial des poi
+        val position=LatLng(51.5145160, -0.1270060)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
+    }
+
+    //On simule ici le remplissage des points d’interets
+    private fun addItems() {
+
+        // Set some lat/lng coordinates to start with.
+        var lat = 51.5145160
+        var lng = -0.1270060
+
+        // Add ten cluster items in close proximity, for purposes of this example.
+        for (i in 0..9) {
+            val offset = i / 60.0
+            lat += offset
+            lng += offset
+            val offsetItem =
+                stations.add(Station("Title $i", LatLng(lat,lng),"Adresse $i"))
+        }
+    }
+
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer.
+        val clusterManager = ClusterManager<Station>(this, googleMap)
+        clusterManager.renderer =
+            StationRenderer(
+                this,
+                googleMap,
+                clusterManager
+            )
+
+        // Add the places to the ClusterManager.
+        addItems()
+        clusterManager.addItems(stations)
+        clusterManager.cluster()
+
+        // Set ClusterManager as the OnCameraIdleListener so that it
+        // can re-cluster when zooming in and out.
+        googleMap.setOnCameraIdleListener {
+            clusterManager.onCameraIdle()
+        }
+        val position=LatLng(51.5145160, -0.1270060)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
     }
 }
--- app/src/main/java/com/example/googlemap/Station.kt
+++ app/src/main/java/com/example/googlemap/Station.kt
@@ -1,7 +1,15 @@
 package com.example.googlemap
 
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
 
 data class Station(val name: String,
                    val latLng: LatLng,
-                   val address: String)
+                   val address: String) : ClusterItem {
+    override fun getPosition(): LatLng =
+        latLng
+    override fun getTitle(): String =
+        name
+    override fun getSnippet(): String =
+        address
+}
